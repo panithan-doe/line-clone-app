1c7ab08af043f08cb93a5e78aba90ffbb0aa1007
Panithan Champahom <doedoserpc@gmail.com>
2025-07-08 11:26:19 +0700

    Add amplify.yml

--- src/lib/amplify.ts
+++ src/lib/amplify.ts
@@ -1,7 +1,7 @@
 import { Amplify } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/data';
 import type { Schema } from '../../amplify/data/resource';
-// import outputs from '../../amplify_outputs.json';
+// import outputs from '../amplify_outputs.json';
 
 // Amplify.configure(outputs);
 
